ccf066c6df6721602ec521d685da0e164d0d1795
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2020-04-17 16:14:04 -0400

    Continued to build server.js and added layout for handlebars

--- server.js
+++ server.js
@@ -5,12 +5,25 @@
 
 //Imports
 const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 
 //Set PORT
 let PORT = process.env.PORT || 3000;
 
 //Init Express
 const app = express();
+
+//body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+//express.static to serve public folder as static directory
+app.use(express.static("public"));
+
+//Handlebars
+app.engine("hadlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
 
 //Start the server
 app.listen(PORT, ()=>{
